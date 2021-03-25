c4faf5fd1cbd57b82a265f9c62520728a5afe626
sub-nautic <szymon.poradzewski@gmail.com>
2021-03-25 23:31:52 +0100

    Add Shooting with projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -1,12 +1,14 @@
 using System;
+using RPG.Core;
 using UnityEngine;
 
 namespace RPG.Combat
 {
     public class Projectile : MonoBehaviour
     {
-        [SerializeField] Transform target = null;
         [SerializeField] float speed = 1f;
+
+        Health target = null;
 
         void Update()
         {
@@ -16,15 +18,20 @@ namespace RPG.Combat
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
+        public void SetTarget(Health target)
+        {
+            this.target = target;
+        }
+
         Vector3 GetAimLocation()
         {
-            //protection to shooting in foots of target
+            //set where projectile should hit on the target
             CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
             if(targetCapsule == null)
             {
-                return target.position;
+                return target.transform.position;
             }
-            return target.position + Vector3.up * targetCapsule.height / 2;
+            return target.transform.position + Vector3.up * targetCapsule.height / 1.5f;
         }
     }
 }
