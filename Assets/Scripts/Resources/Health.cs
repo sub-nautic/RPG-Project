e7e5d1360414ba046ed20444082d82132860897d
sub-nautic <szymon.poradzewski@gmail.com>
2021-03-29 22:16:24 +0200

    Add Finding a stat by enum

--- Assets/Scripts/Resources/Health.cs
+++ Assets/Scripts/Resources/Health.cs
@@ -14,7 +14,7 @@ namespace RPG.Resources
 
         void Start()
         {
-            healthPoints = GetComponent<BaseStats>().GetHealth();
+            healthPoints = GetComponent<BaseStats>().GetStat(Stat.Health);
         }
 
         public bool IsDead()
@@ -35,7 +35,7 @@ namespace RPG.Resources
 
         public float GetPercentage()
         {
-            return 100 * (healthPoints / GetComponent<BaseStats>().GetHealth());
+            return 100 * (healthPoints / GetComponent<BaseStats>().GetStat(Stat.Health));
         }
 
         void AwardExperience(GameObject instigator)
@@ -43,7 +43,7 @@ namespace RPG.Resources
             Experience experience = instigator.GetComponent<Experience>();
             if(experience == null) return;
 
-            experience.GainExperience(GetComponent<BaseStats>().GetExperienceReward());
+            experience.GainExperience(GetComponent<BaseStats>().GetStat(Stat.ExperienceReward));
         }
 
         void Die()
