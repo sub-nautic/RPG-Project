541eab837c03882c6aa37604c4f9d7db3b28f83d
sub-nautic <szymon.poradzewski@gmail.com>
2021-04-01 01:33:31 +0200

    Add Modifier system (exensible)

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
 using RPG.Core;
@@ -7,7 +8,7 @@ using RPG.Stats;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour, IAction, ISaveable
+    public class Fighter : MonoBehaviour, IAction, ISaveable, IModifierProvider
     {
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] Transform rightHandTransform = null;
@@ -121,6 +122,14 @@ namespace RPG.Combat
         {
             GetComponent<Animator>().ResetTrigger("attack");
             GetComponent<Animator>().SetTrigger("stopAttack");
+        }
+
+        public IEnumerable<float> GetAdditiveModifier(Stat stat)
+        {
+            if(stat == Stat.Damage)
+            {
+                yield return currentWeapon.GetDamage();
+            }
         }
 
         public object CaptureState()
---
+++ Assets/Scripts/Stats/IModifierProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RPG.Stats
+{
+    public interface IModifierProvider
+    {
+        //IEnumerable allows to use Enumerators in foreach loops
+        IEnumerable<float> GetAdditiveModifier(Stat stat);
+    }
+}
